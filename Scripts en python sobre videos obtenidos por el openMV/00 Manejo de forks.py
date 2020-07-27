de4726ac789f968b769496bf1238da4ea0e3691b
Javier Kipen <javier1kipen@gmail.com>
2020-07-27 19:59:45 -0300

    Creacion scripts python
    Se grabaron videos con el openMV para procesarlos con la computadora

---
+++ Scripts en python sobre videos obtenidos por el openMV/00 Manejo de forks.py
@@ -0,0 +1,12 @@
+import cv2
+
+cap = cv2.VideoCapture('Videos/Fork 640x480.mp4')
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if ret == True:
+        cv2.imshow('Frame', frame)
+        cv2.waitKey(33)
+    else:
+        break
+cap.release()
+cv2.destroyAllWindows()
