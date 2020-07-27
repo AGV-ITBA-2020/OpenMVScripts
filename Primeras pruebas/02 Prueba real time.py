f124a570326ce63931ea2947c35c8cf09bf69100
Javier Kipen <javier1kipen@gmail.com>
2020-07-27 17:44:33 -0300

    Script para probar si corre en real time
    Este script permite ver si una funciòn puede correr en tiempo real. Además tiene el link con instrucciones para dejarle el programa al bicho y que no mande paquetes a la pc

---
+++ Primeras pruebas/02 Prueba real time.py
@@ -0,0 +1,33 @@
+# Untitled - By: Javier - Mon Jul 27 2020
+
+import sensor, image, time, utime
+from pyb import LED
+
+
+sensor.reset()
+sensor.set_pixformat(sensor.RGB565) # or GRAYSCALE...
+sensor.set_framesize(sensor.QVGA) # or QQVGA...
+sensor.skip_frames(time = 2000)
+#clock = time.clock()
+
+FPS=18
+red_led=LED(1); # Uso los leds para ver si el programa corre en tiempo real
+green_led=LED(2);
+blue_led=LED(3);
+
+while(True):
+    #clock.tick()
+
+    start = utime.ticks_ms()
+    img = sensor.snapshot().cartoon(seed_threshold=0.1, floating_thresholds=0.05) #Pongo la función de cartoon para que consuma un poco de procesamiento
+
+    t_elapsed = utime.ticks_diff(utime.ticks_ms(), start)
+    if t_elapsed/1000 > (1/FPS): #Si la tarea tardó más de 1/FPS se pone en rojo
+        green_led.off()
+        red_led.on()
+    else: #Sino en verde
+        green_led.on()
+        red_led.off()
+
+    #print(clock.fps(), t_elapsed, 1/FPS )
+#https://forums.openmv.io/viewtopic.php?t=689 Para correr el programa sin pasar info a la pc
